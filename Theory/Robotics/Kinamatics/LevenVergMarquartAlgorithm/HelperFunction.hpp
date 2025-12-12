dca5512f4b0426ca25d4f390a23c11ad8648ea78
chenhuaming <337467729@qq.com>
2025-12-12 16:49:45 +0800

    change submodule path

--- Theory/Robotics/Kinamatics/LevenVergMarquartAlgorithm/HelperFunction.hpp
+++
@@ -1,7 +0,0 @@
-#include <cmath>
-
-template<typename Scalar>
-inline Scalar constrain(const Scalar val,const Scalar min,const Scalar max)
-{
-    return val < min ? min : (val > max ? max : val);
-}
